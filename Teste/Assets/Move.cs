d10c26013e85461a2f07b00ed972d5b337d33805
Daniel Ohata <danthyl@gmail.com>
2021-03-08 08:30:39 -0300

    Release

---
+++ Teste/Assets/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 goal = new Vector3(5,0,4);
+    //Vector3 goal = new Vector3(5, 0, 4);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal = goal * 0.01f;
+        //this.transform.Translate(goal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(goal);
+    }
+}
---
+++ githubteste/Assets/Scripts/Carro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carro : MonoBehaviour
+{
+    public Vector3 goal = new Vector3(0,0,0);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal = goal * 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(goal);
+    }
+}
+
